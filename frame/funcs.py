cd90228f23d58a2d9252c663a74161a539ee99db
Intelektika team <kvantorium73.int@gmail.com>
2025-11-03 02:11:47 +0400

    Update funcs.py

--- frame/funcs.py
+++ frame/funcs.py
@@ -173,22 +173,3 @@ result: 34
         print(f"Error in code running: {e}")
         return None
 
-
-class FramerError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class FrameApiError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class FramingError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class PluginIsNotWorkingError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class PluginError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
-class FrameExecutionError(Exception):
-    def __init__(self, *args):
-        super().__init__(*args)
