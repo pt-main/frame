8ae462dbd23879d33d43ede2bfe49cd631b87488
pt-main <pt.main.acc@gmail.com>
2025-11-03 23:30:47 +0400

    no message

--- frame/simple.py
+++ frame/simple.py
@@ -1,3 +1,9 @@
+'''
+# Simple Frames Core
+Simplified module of frame concept for beginers.
+'''
+
+
 from .frame_core import (Frame, Framer, framing, framing_result, fExec, fGet, fVar, fSys)
 from .frame_core.exceptions import *
 from .frame_core.plugins import (MathPlugin)
